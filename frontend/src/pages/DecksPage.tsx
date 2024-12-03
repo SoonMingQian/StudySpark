f28b3c8f02d54d1d5af6bf4db29ae06dc34928a6
G00438454 <g00438454@atu.ie>
2024-12-03 22:46:38 +0000

    removing unused Flashcard import

--- frontend/src/pages/DecksPage.tsx
+++ frontend/src/pages/DecksPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Flashcard , Deck} from '../type';
+import { Deck} from '../type';
 
 interface DecksPageProps {
     decks: Deck[];
